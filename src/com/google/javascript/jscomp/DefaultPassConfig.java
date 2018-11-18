30d738371e92e9f304a332a8d548f7f00654aaf7
Chad Killingsworth <chadkillingsworth@gmail.com>
2018-11-17 19:25:55 -0600

    Diable ambiguate properties due to ES6 mixin bug

--- src/com/google/javascript/jscomp/DefaultPassConfig.java
+++ src/com/google/javascript/jscomp/DefaultPassConfig.java
@@ -766,11 +766,11 @@ public final class DefaultPassConfig extends PassConfig {
       passes.add(extractPrototypeMemberDeclarations);
     }
 
-    if (options.shouldAmbiguateProperties()
-        && options.propertyRenaming == PropertyRenamingPolicy.ALL_UNQUOTED
-        && options.isTypecheckingEnabled()) {
-      passes.add(ambiguateProperties);
-    }
+//    if (options.shouldAmbiguateProperties()
+//        && options.propertyRenaming == PropertyRenamingPolicy.ALL_UNQUOTED
+//        && options.isTypecheckingEnabled()) {
+//      passes.add(ambiguateProperties);
+//    }
 
     if (options.propertyRenaming == PropertyRenamingPolicy.ALL_UNQUOTED) {
       passes.add(renameProperties);
